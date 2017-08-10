41cd09b2f1dc5c8321e554601ca77d01cd4fb0c4
Yifan Guo <yguo14@ncsu.edu>
2017-08-10 13:02:23 -0400

    kept processing

--- source_data.py
+++ source_data.py
@@ -1,8 +1,8 @@
 import xlrd
 
 
-def read_sheet():
-    wkb = xlrd.open_workbook("Workbook1.xlsx")
+def read_sheet(sheet_name):
+    wkb = xlrd.open_workbook(sheet_name)
     sheet = wkb.sheet_by_index(0)
 
     _matrix = []
@@ -20,30 +20,41 @@ def read_sheet():
     return _matrix
 
 
-_matrix = read_sheet()
-
-
-def within_label(n):
+def within_index(n, x, y):
+    if x < n < y:
+        return True
     return False
 
 
-def find_label(list):
+def find_label(label_list):
+    if label_list[3] == '' and label_list[4] == '' and label_list[5] == '':
+        return True
     return False
 
 
 def get_all_labeled(_matrix):
     n = len(_matrix)
     m = len(_matrix[0])
-    all_projects = {}
+    data = []
     for i in range(n):
-        tmp = []
+        features = []
         labels = []
         for j in range(m):
             cell = _matrix[i][j]
-            if within_label(j):
+            if within_index(j, 2, 10):
                 labels.append(cell)
             else:
-                tmp.append(cell)
+                features.append(cell)
         label = find_label(labels)
-        all_projects[tmp] = label
-    return all_projects
+        features.append(label)
+        data.append(features)
+    return data
+
+
+matrix = read_sheet("Workbook1.xlsx")
+
+print matrix[0]
+
+all_projects = get_all_labeled(matrix)
+
+print all_projects[0]
